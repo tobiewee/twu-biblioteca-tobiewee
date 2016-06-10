207ed7b4848aa36e09e8f0d695650eea85fca475
Tobie Wee <tobiewee@gmail.com>
2016-06-10 14:50:12 +0800

    Welcome Message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    public void displayWelcomeMessage() {
+        System.out.print("Welcome to Biblioteca!\n");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,43 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    private BibliotecaApp app;
+    private PrintStream originalOutStream;
+    private ByteArrayOutputStream testOutStream;
+
+    @Before
+    public void setUp() {
+        app = new BibliotecaApp();
+        originalOutStream = System.out;
+        testOutStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOutStream));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOutStream);
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testDisplayWelcomeMessage() {
+        String expectedMessage = "Welcome to Biblioteca!\n";
+        app.displayWelcomeMessage();
+        String outputMessage =  testOutStream.toString();
+        assertEquals(expectedMessage, outputMessage);
+    }
+
+    @Test
+    public void testListBooks() {
+        assertEquals(0, app.bookList.size());
     }
 }
